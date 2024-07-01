e4a688b239133f5af2cbd9a8fabc2da6e11f96a3
Adelton-Silva <adeltonair@gmail.com>
2024-07-01 17:15:08 +0100

    dashboard change12

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,12 +11,9 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"><img src="assets/images/4.png" height="600" width="500" /></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Bem vindo de volta!</h1>
-                                    </div>
                                     <form class="user" method="POST" action="{{ route('login') }}">
                                         @csrf
                                         <div class="form-group">
@@ -92,24 +89,4 @@
             </div>
         </div>
     </div>
-
-            @section('scripts')
-                <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-                <link
-                    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-                    rel="stylesheet">
-
-                <!-- Custom styles for this template-->
-                <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
-                <!-- Bootstrap core JavaScript-->
-                <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-                <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-                <!-- Core plugin JavaScript-->
-                <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-                <!-- Custom scripts for all pages-->
-                <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
-            @endsection
-        @endsection
+@endsection
